eff92b4e4abf4825536a0ac0eb2e9eaf93348c66
dohyeon-Lee <dohyeon@postech.ac.kr>
2026-06-15 22:25:15 +0900

    save

--- sync_from_server.sh
+++ sync_from_server.sh
@@ -27,6 +27,8 @@ remote_quote() {
 }
 
 SYNC_ROOT_PATTERNS=(
+    "dataset"
+    "dataset_*"
     "libero_dataset"
     "libero_small_dataset"
     "libero_10_old"
--- sync_to_server.sh
+++ sync_to_server.sh
@@ -24,6 +24,7 @@ die() {
 
 SYNC_ROOT_PATTERNS=(
     "dataset"
+    "dataset_*"
     "libero_dataset"
     "libero_small_dataset"
     "libero_10_old"
